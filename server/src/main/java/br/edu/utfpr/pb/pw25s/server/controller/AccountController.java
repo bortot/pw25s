9444e8fcbd5d97465552d11ac0028e9ffb7b4af5
Abel Ronsoni <bortotabel@gmail.com>
2023-10-10 20:52:57 -0300

    create controllers

---
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/controller/AccountController.java
@@ -0,0 +1,37 @@
+package br.edu.utfpr.pb.pw25s.server.controller;
+
+import br.edu.utfpr.pb.pw25s.server.dto.UserDTO;
+import br.edu.utfpr.pb.pw25s.server.model.User;
+import br.edu.utfpr.pb.pw25s.server.service.UserService;
+import br.edu.utfpr.pb.pw25s.server.shared.GenericResponse;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("account")
+public class AccountController {
+
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    public AccountController(UserService userService,
+                             ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping
+    public GenericResponse createUser(@Valid @RequestBody UserDTO user) {
+        User userEntity = modelMapper.map(user, User.class);
+        userService.save(userEntity);
+
+        GenericResponse genericResponse = new GenericResponse();
+        genericResponse.setMessage("User saved.");
+        return genericResponse;
+    }
+
+}
