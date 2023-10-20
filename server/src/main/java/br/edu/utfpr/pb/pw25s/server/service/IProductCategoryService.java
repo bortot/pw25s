0280ed2fd21e33049c14fee6c726e12821cd188d
Abel Ronsoni <bortotabel@gmail.com>
2023-10-19 21:59:30 -0300

    remove product

--- server/src/main/java/br/edu/utfpr/pb/pw25s/server/service/IProductCategoryService.java
+++
@@ -1,6 +0,0 @@
-package br.edu.utfpr.pb.pw25s.server.service;
-
-import br.edu.utfpr.pb.pw25s.server.model.ProductCategory;
-
-public interface IProductCategoryService extends ICrudService<ProductCategory, Long> {
-}
