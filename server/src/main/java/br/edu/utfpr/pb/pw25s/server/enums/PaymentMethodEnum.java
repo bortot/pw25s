20f71de1456496e4bae928ae2bc17535807b4c44
Abel Ronsoni <bortotabel@gmail.com>
2023-10-19 22:00:50 -0300

    remove PaymentMethodEnum and add PaymentMethod class

--- server/src/main/java/br/edu/utfpr/pb/pw25s/server/enums/PaymentMethodEnum.java
+++
@@ -1,9 +0,0 @@
-package br.edu.utfpr.pb.pw25s.server.enums;
-
-public enum PaymentMethodEnum {
-    CREDIT_CARD,
-    DEBIT_CARD,
-    MONEY,
-    PIX,
-    CHEQUE;
-}
---
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/PaymentMethod.java
@@ -0,0 +1,31 @@
+package br.edu.utfpr.pb.pw25s.server.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment_method")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter @Setter
+public class PaymentMethod {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 50)
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
