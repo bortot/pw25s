8556283eace2b3c85bc89c72bc93711df62f8452
Abel Ronsoni <bortotabel@gmail.com>
2023-10-23 20:22:22 -0300

    implements the controller class, and adds the pre save method to store the logged user

--- server/src/main/java/br/edu/utfpr/pb/pw25s/server/controller/AccountController.java
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/controller/AccountController.java
@@ -1,10 +1,45 @@
 package br.edu.utfpr.pb.pw25s.server.controller;
 
+import br.edu.utfpr.pb.pw25s.server.dto.AccountDTO;
+import br.edu.utfpr.pb.pw25s.server.model.Account;
+import br.edu.utfpr.pb.pw25s.server.model.User;
+import br.edu.utfpr.pb.pw25s.server.service.IAccountService;
+import br.edu.utfpr.pb.pw25s.server.service.ICrudService;
+import br.edu.utfpr.pb.pw25s.server.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("account")
-public class AccountController {
+public class AccountController extends CrudController<Account, AccountDTO, Long> {
 
+    private static IAccountService iAccountService;
+    private static  ModelMapper modelMapper;
+    private static  UserService userService;
+
+    public AccountController(IAccountService iAccountService ,ModelMapper modelMapper, UserService userService) {
+        super(Account.class, AccountDTO.class);
+        AccountController.iAccountService = iAccountService;
+        AccountController.modelMapper = modelMapper;
+        AccountController.userService = userService;
+    }
+
+    @Override
+    protected ICrudService getService() {
+        return iAccountService;
+    }
+
+    @Override
+    protected ModelMapper getModelMapper() {
+        return modelMapper;
+    }
+
+    @Override
+    public Account preSave(Account entity) {
+        User user = userService.getLoggedUser();
+        entity.setUser(user);
+
+        return super.preSave(entity);
+    }
 }
